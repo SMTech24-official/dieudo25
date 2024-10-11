c5d7e7186518bf42a6376a31fb608c1f13b28a3a
Tahsin0909 <tajisan789@gmail.com>
2024-10-11 15:11:10 +0600

    Added banner

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -17,41 +17,51 @@ const Hero = () => {
     return (
         <div className="bg-[#003366]">
             <div className=" max-w-[1700px] mx-auto px-4 text-white">
-                <div className="grid grid-cols-2 items-center justify-center lg:p-10">
-                    <div className="space-y-8">
-                        <div className="space-y-2">
-                            <h2 className="text-5xl font-bold ">
-                                Find trusted garages near you, <br />
+                <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-0 md:gap-10 gap-4 items-center justify-center lg:p-10 md:py-8 py-5">
+                    <div className="space-y-8 ">
+                        <div className="space-y-2 ">
+                            <h2 className="text-2xl md:text-3xl lg:text-5xl font-bold lg:w-[700px]">
+                                Find trusted garages near you, 
                                 in just a few clicks
                             </h2>
-                            <h4>
+                            <h4 className="text-sm md:text-base">
                                 Get a quote or book a service from
                                 top rated garages instantly for your
                                 tire or maintenance.
                             </h4>
                         </div>
                         <form className="max-w-md" onSubmit={(e) => handleSearch(e)}>
-                            <input
-                                name="search"
-                                type="search"
-                                placeholder="Search for any service..."
-                                className="w-full px-4 py-2 rounded bg-gray-800 text-black placeholder-gray-400"
-                                required
-                            />
+                            <div className="flex items-center gap-2 relative">
+                                <label htmlFor="Search" className="md:text-xl">Search:</label>
+                                <input
+                                    id="Search"
+                                    name="search"
+                                    type="search"
+                                    placeholder="Search for any service..."
+                                    className="w-full peer px-4 py-2 rounded bg-gray-800 text-black placeholder-gray-400 bg-[#003366] border-b border-[#003366] outline-none focus:border-white"
+                                    required
+                                />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-map-pin absolute right-0 opacity-35 peer-focus:opacity-100">
+                                    <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                                    <circle cx="12" cy="10" r="3" />
+                                </svg>
+                            </div>
+
+
                             <button
                                 type="submit"
-                                className="mt-4 bg-[#FF6600] hover:bg-[#FF6600]/80 text-black w-full py-2 rounded"
+                                className="mt-4 bg-[#FF6600] hover:bg-[#FF6600]/80  w-full py-2 rounded text-white"
                             >
                                 Find garages
                             </button>
                         </form>
                     </div>
-                    <div className="w-full h-[60vh]">
+                    <div className="w-full h-[60vh] ">
                         <Image
                             src={image4}
                             alt="Banner Image"
                             objectFit="cover"
-                            className="rounded-lg !w-full !h-full object-cover"
+                            className="rounded-lg !w-full !h-full object-cover "
                         />
                     </div>
                 </div>
