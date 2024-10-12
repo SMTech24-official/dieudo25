f56d335e4b563f33542d016252899eebde60f50d
Tahsin0909 <tajisan789@gmail.com>
2024-10-12 16:11:56 +0600

    Updated garage card button

--- src/components/home/FeaturedGarages.tsx
+++ src/components/home/FeaturedGarages.tsx
@@ -78,8 +78,11 @@ export default function FeaturedGarages() {
                     </span>
                   </div>
                 </div>
-                <Button variant="default" className="w-full bg-secondary">
-                  Get a Quote
+                <Button
+                  className="mt-4 flex items-center group gap-2 bg-secondary hover:bg-secondary/80 active:scale-95 transition-all w-full py-2 rounded text-white"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-wrench group-hover:fill-white"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>
+                  <span>Book Now </span>
                 </Button>
               </CardContent>
             </Card>
